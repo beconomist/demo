66310fd9a2bfae968a855633adf38dfa26d95415
beconomist <beconomist@gmail.com>
2017-03-12 02:22:58 +0800

    refactor index.js

--- index.js
+++ index.js
@@ -1,17 +1,42 @@
+// 寫ES6語法需用到 babel
 require('babel-register');
-var express = require('express');
-var app = express();
-var http = require('http');
-var fs = require('fs');
-var path = require('path');
-var jquery = require('jquery');
-var meetup = require('meetup-api')({
+const express = require('express');
+const app = express();
+const meetup = require('meetup-api')({
+  // meetup.com api key
   key: '5c5c4e33c47164782d7b474a334830'
 });
+let numberOfRequest = 0;
+const router = express.Router();
+
+// 這個 middleware 會紀錄所有 request 的次數、時間、url和 method，
+// 但是沒有 end the request-response cycle，所以要 call next
+app.use(function(req, res, next){
+  console.log('Server is requested: ' + (++numberOfRequest) + ' time(s)');
+  console.log('Requested time: %d', Date.now());
+  console.log('Request URL: ' , req.originalUrl);
+  console.log('Request Type: ' , req.method);
+  console.log('---');
+  // Call next()，將 control 交給下一個 middleware
+  next();
+});
+
+// 設定 static resources 的檔案夾
+// express 會直接從 public 檔案夾 load，所以 public 並不會成為 URL 的一部分
+app.use(express.static('public'));
 
 // 首頁
-app.get('/', function(req, res) {
-  res.sendFile('./public/index.html', { root: __dirname });
+app.get('/', function(req, res, next) {
+    // example middleware: something to do here
+    console.log('middleware 1');
+    next();
+  }, function(req, res, next) {
+    // example middleware: something to do here
+    console.log('middleware 2');
+    next();
+  }, function(req, res) {
+    res.sendFile('index.html', { root: __dirname });
+    console.log('---');
   });
 
 // 取得附近 meetups
@@ -33,17 +58,24 @@ app.get('/meetups', function(req, res) {
     res.send(html);
 
   });
-  //
+
 });
-
-
-// 未設定這個，index.html就無法讀到jQuery library
-app.use(express.static('public'));
 
 app.get('/posts', function(req, res) {
   res.sendFile('./db/blogPosts.json', {  root:__dirname });
 });
 
+router.get('/', function(req, res, next) {
+  console.log('do something');
+  next();
+});
+
+app.use('/admin', router, function(req, res) {
+  res.sendStatus(401);
+});
+
+// 當 app 在 herokuapp 等平台上執行時 port 是動態變化的，
+// 所以 default 用 process.evn.PORT
 app.listen(process.env.PORT || 3000, function() {
-  console.log('Server listening now');
+  console.log('Server listening now' );
 });
